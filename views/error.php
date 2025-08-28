3aa7f45cf92854f1b1ba8f0a7b45d9483cdd43c1
0xdac <darienalonso@gmail.com>
2025-08-28 18:12:55 +0200

    Agregar un vista para mostrar en caso de error

---
+++ views/error.php
@@ -0,0 +1,15 @@
+<?php
+?>
+<div>
+    <h3 style="color:red;">Error</h3>
+    <h1>La página solicitada no se encuentra disponible</h1>
+
+    <p>Intente regresar a la página de <a class="#" href="index.php">Inicio</a></p>
+    <p></p>
+</div>
+
+
+
+
+
+
